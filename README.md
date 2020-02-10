03ee232e8ef2b4a734128b86d7ff42b8ca9b33b3
luis_parra <luis.parra.pulido@gmail.com>
2020-02-10 14:21:53 -0500

    Preparando entorno y desplegando en now

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Curso_React_Avanzado
+# curso-platzi-react-avanzado ⚛️
+
+Repositorio con el código del [Curso Avanzado de React de Platzi](https://platzi.com/cursos/react-avanzado/)
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  output: {
+    filename: 'app.bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+}
