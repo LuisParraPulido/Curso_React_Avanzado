63a41c8212dcecf3bf4c226b8da53696c79450a4
luis_parra <luis.parra.pulido@gmail.com>
2020-02-11 15:01:10 -0500

    Hooks useState, useEffect

--- src/components/ListOfCategorys/index.js
+++ src/components/ListOfCategorys/index.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Category } from '../Category'
-
 import { List, Item } from './styles'
-import { categories } from '../../../api/db.json'
 
 export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([])
+
+  useEffect(function () {
+    window.fetch('https://petgram-server-luis-qucwycsec.now.sh/categories')
+      .then(res => res.json())
+      .then(response => setCategories(response))
+  }, [])
+
   return (
     <List>
       {
