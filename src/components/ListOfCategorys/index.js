ef4148e98ef19f513de6db4c19bdff7a56e8bb07
luis_parra <luis.parra.pulido@gmail.com>
2020-02-12 08:41:56 -0500

    UseEffect: limpiando eventos

--- src/components/ListOfCategorys/index.js
+++ src/components/ListOfCategorys/index.js
@@ -4,6 +4,7 @@ import { List, Item } from './styles'
 
 export const ListOfCategories = () => {
   const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)
 
   useEffect(function () {
     window.fetch('https://petgram-server-luis-qucwycsec.now.sh/categories')
@@ -11,11 +12,29 @@ export const ListOfCategories = () => {
       .then(response => setCategories(response))
   }, [])
 
-  return (
-    <List>
+  useEffect(function () {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
       {
         categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
       }
     </List>
+  )
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
