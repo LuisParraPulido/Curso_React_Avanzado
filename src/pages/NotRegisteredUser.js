591758a83613499bf1a57df7fcfce15e2089b46d
luis_parra <luis.parra.pulido@gmail.com>
2020-02-14 08:14:27 -0500

    Mutaciones para el registro

--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,17 +1,29 @@
 import React from 'react'
 import Context from '../Context'
 import { UserForm } from '../components/UserForm'
+import { RegisterMutation } from '../container/RegisterMutation'
 
 export const NotRegisteredUser = () => (
   <Context.Consumer>
     {
       ({ activateAuth }) => {
-        return (
-          <>
-            <UserForm title='Registrarse' onSubmit={activateAuth} />
-            <UserForm title='Iniciar sesión' onSubmit={activateAuth} />
-          </>
-        )
+        return <>
+          <RegisterMutation>
+            {
+              (register) => {
+                const onSubmit = ({ email, password }) => {
+                  const input = { email, password }
+                  const variables = { input }
+                  register({ variables }).then(activateAuth)
+                }
+
+                return <UserForm title='Registrarse' onSubmit={onSubmit} />
+              }
+            }
+          </RegisterMutation>
+
+          <UserForm title='Iniciar sesión' onSubmit={activateAuth} />
+        </>
       }
     }
   </Context.Consumer>
